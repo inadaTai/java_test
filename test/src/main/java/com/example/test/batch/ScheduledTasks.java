8914dcf1aa40a343655b881b3946f5c8b10160c7
inadaTai <starmelt190@gmail.com>
2023-03-14 11:20:48 +0900

    イニシャライズ予定のデータロジックをdataseedに移行とスケジュールタスクを毎月1日に実行するようにしました。

--- test/src/main/java/com/example/test/batch/ScheduledTasks.java
+++ test/src/main/java/com/example/test/batch/ScheduledTasks.java
@@ -1,15 +1,13 @@
 package com.example.test.batch;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.io.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.example.test.repository.PersonRepository;
 import com.example.test.entity.PersonEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Component
 
@@ -17,42 +15,17 @@ public class ScheduledTasks {
 
 	private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
 
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-    
     @Autowired   
     PersonRepository personRepository; 
     
     /**
      * @see https://qiita.com/rubytomato@github/items/4f0c64eb9a24eaceaa6e
-     * 毎日定期実行されるタスクです。
+     * 毎月1日に定期実行されるタスクです。
      * cronについては上記を参考にした。
     */
-    @Scheduled(fixedDelay = 10000)
-	public void reportCurrentTime() {
-        // BufferedReader br = null;
-        // String data_csv = "src/main/resources/csv/user-data.csv";
-        // try {
-        //     File file = new File(data_csv);
-        //     br = new BufferedReader(new FileReader(file));
-        //     String line;
-        //     String[] data;
-        //     br.readLine();
-        //     Integer cnt = 1;
-        //     while ((line = br.readLine()) != null) {
-        //         data = line.split(",");
-        //         PersonEntity newEntity = new PersonEntity(cnt, data[0], data[1]);
-        //         personRepository.save(newEntity);
-        //         cnt++;
-        //     }
-        // } catch (Exception e) {
-        //     System.out.println(e.getMessage());
-        // } finally {
-        //     try {
-        //         br.close();
-        //     } catch (Exception e) {
-        //         System.out.println(e.getMessage());
-        //     }
-        // }
+    @Scheduled(cron = "0 0 0 1 * *", zone = "Asia/Tokyo")
+	public void monthlyBatch() {
+        log.info("monthly_data");
         return ;
 	}
 }
---
+++ test/src/main/java/com/example/test/dataseed/PersonSeeder.java
@@ -0,0 +1,63 @@
+package com.example.test.dataseed;
+
+import java.io.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.example.test.repository.PersonRepository;
+import com.example.test.entity.PersonEntity;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Component
+public class PersonSeeder implements CommandLineRunner {
+
+    private static final Logger log = LoggerFactory.getLogger(PersonSeeder.class);
+    
+    @Autowired   
+    PersonRepository personRepository; 
+
+    @Override
+	public void run(String... args) throws Exception {
+		loadPersonData();
+	}
+    
+    /**
+     * @see https://qiita.com/rubytomato@github/items/4f0c64eb9a24eaceaa6e
+     * 毎日定期実行されるタスクです。
+     * cronについては上記を参考にした。
+    */
+	private void loadPersonData() {
+        log.info("start seeder");
+        BufferedReader br = null;
+        String data_csv = "src/main/resources/csv/user-data.csv";
+        try {
+            File file = new File(data_csv);
+            br = new BufferedReader(new FileReader(file));
+            String line;
+            String[] data;
+            br.readLine();
+            List<PersonEntity> persons = personRepository.findAll();
+            Integer person_cnt = persons.size();
+            if(person_cnt != 0){
+                log.info("already exist person data");
+                return ;
+            }
+            while ((line = br.readLine()) != null) {
+                data = line.split(",");
+                PersonEntity newEntity = new PersonEntity(data[0], data[1]);
+                personRepository.save(newEntity);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        } finally {
+            try {
+                br.close();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        return ;
+	}
+}
